ab40d3c58120822605f5fbccbe1cbd59cafc9039
Maxime Deravet <maximed@uw.edu>
2017-08-03 10:54:28 -0700

    Initial Commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { ContentServicesUiPage } from './app.po';
+
+describe('content-services-ui App', () => {
+  let page: ContentServicesUiPage;
+
+  beforeEach(() => {
+    page = new ContentServicesUiPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
