b13c162e26cebc626567fba2fdd777f653529923
Andrew Gagne <agagne@uw.edu>
2017-09-07 09:18:25 -0700

    CAB-2562: add subscription unsubscribe to editPage's onDestroy

--- src/app/pages/edit-page/edit-page.component.ts
+++ src/app/pages/edit-page/edit-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { EditPageConfig } from '../../model/config/edit-page-config';
 import { ActivatedRoute } from '@angular/router';
 
@@ -6,13 +6,14 @@ import { ContentService } from '../../services/content.service';
 import { ContentResult } from '../../model/content-result';
 import { Config } from '../../model/config/config';
 import { Title } from '@angular/platform-browser';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.css']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
   config: Config;
   pageConfig: EditPageConfig;
   page: string;
@@ -20,6 +21,8 @@ export class EditPageComponent implements OnInit {
 
   contentItem: ContentResult;
   viewFileUrl: string;
+
+  private readSubscription: Subscription;
 
   constructor(private route: ActivatedRoute, private titleService: Title, private contentService: ContentService) {}
 
@@ -35,9 +38,17 @@ export class EditPageComponent implements OnInit {
         this.pageConfig = data.config.pages[this.page.toLowerCase()].editPageConfig;
         this.titleService.setTitle(this.pageConfig.pageName);
 
-        this.contentService.read(this.id).subscribe(contentItem => (this.contentItem = contentItem));
+        this.readSubscription = this.contentService
+          .read(this.id)
+          .subscribe(contentItem => (this.contentItem = contentItem));
         this.viewFileUrl = this.contentService.getFileUrl(this.id, true);
       });
     });
+  }
+
+  ngOnDestroy(): void {
+    console.log('unsubscribe edit page - read subscription');
+    // prevent memory leak when component destroyed
+    this.readSubscription.unsubscribe();
   }
 }
