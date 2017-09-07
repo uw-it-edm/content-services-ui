ecb40483ee8ba7ebaa692322fe0949f832bf6f23
Andrew Gagne <agagne@gmail.com>
2017-09-07 10:53:01 -0700

    Feature/cab 2562 (#17)
    * CAB-2562: replace unsubscription with takeUntil pattern

--- src/app/pages/edit-page/edit-page.component.ts
+++ src/app/pages/edit-page/edit-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { EditPageConfig } from '../../model/config/edit-page-config';
 import { ActivatedRoute } from '@angular/router';
 
@@ -6,13 +6,15 @@ import { ContentService } from '../../services/content.service';
 import { ContentResult } from '../../model/content-result';
 import { Config } from '../../model/config/config';
 import { Title } from '@angular/platform-browser';
+import 'rxjs/add/operator/takeUntil';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.css']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
   config: Config;
   pageConfig: EditPageConfig;
   page: string;
@@ -21,12 +23,14 @@ export class EditPageComponent implements OnInit {
   contentItem: ContentResult;
   viewFileUrl: string;
 
+  private componentDestroyed = new Subject();
+
   constructor(private route: ActivatedRoute, private titleService: Title, private contentService: ContentService) {}
 
   ngOnInit() {
     console.log('init edit page component');
 
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.takeUntil(this.componentDestroyed).subscribe(params => {
       this.page = params.get('page');
       this.id = params.get('id');
 
@@ -35,9 +39,19 @@ export class EditPageComponent implements OnInit {
         this.pageConfig = data.config.pages[this.page.toLowerCase()].editPageConfig;
         this.titleService.setTitle(this.pageConfig.pageName);
 
-        this.contentService.read(this.id).subscribe(contentItem => (this.contentItem = contentItem));
+        this.contentService
+          .read(this.id)
+          .takeUntil(this.componentDestroyed)
+          .subscribe(contentItem => (this.contentItem = contentItem));
         this.viewFileUrl = this.contentService.getFileUrl(this.id, true);
       });
     });
+  }
+
+  ngOnDestroy(): void {
+    // prevent memory leak when component destroyed
+    console.log('unsubscribe edit page');
+    this.componentDestroyed.next();
+    this.componentDestroyed.complete();
   }
 }
