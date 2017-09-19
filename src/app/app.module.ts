32ca28b1ca12548d0b4637c115e7dd1d06233406
James Renfro <jamesrenfro@users.noreply.github.com>
2017-09-19 14:13:03 -0700

    Adding BrowserAnimationsModule to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RoutingModule } from './routing/routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [],
-  imports: [BrowserModule, RoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, RoutingModule],
   exports: [],
   providers: [],
   bootstrap: [AppComponent]
