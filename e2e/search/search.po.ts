bbee05e6b1ebe9f4a77c949249c074403c0b7f34
huiaic <huiaic@uw.edu>
2017-10-06 10:52:34 -0700

    Add protractor tests for page paginators in the search page

--- e2e/search/search.po.ts
+++ e2e/search/search.po.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ExpectedConditions } from 'protractor';
 
 export class SearchPage {
   navigateTo() {
@@ -15,5 +15,37 @@ export class SearchPage {
 
   getSearchBoxInputText() {
     return this.getSearchBox().getAttribute('ng-reflect-model');
+  }
+
+  getPaginators() {
+    return element.all(by.className('mat-paginator'));
+  }
+
+  getPaginatorSelectedTexts() {
+    return element.all(by.className('mat-select-value-text')).getText();
+  }
+
+  clickPaginatorArrow() {
+    element
+      .all(by.className('mat-select-arrow'))
+      .get(0)
+      .click();
+  }
+
+  getPaginatorOptionsTexts() {
+    return element.all(by.className('mat-option')).getText();
+  }
+
+  getPaginatorOptionsPanel() {
+    return element(by.className('mat-select-panel-done-animating'));
+  }
+
+  waitForElementToBeVisible(elem) {
+    browser.wait(ExpectedConditions.visibilityOf(elem), 10000);
+  }
+
+  clickPaginatorSize(index) {
+    const id = 'md-option-' + index;
+    element(by.id(id)).click();
   }
 }
