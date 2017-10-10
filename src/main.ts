e980c63583bbbe3f6ac48c8fc71c2c73a2e6a549
James Renfro <jamesrenfro@users.noreply.github.com>
2017-10-10 09:46:10 -0700

    CAB-2576: Removing commented out code

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-// import {getTranslationProviders} from './app/i18n-providers';
 
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
-
-// getTranslationProviders().then(providers => {
-//   const options = { providers };
-//   platformBrowserDynamic().bootstrapModule(AppModule, options);
-// });
