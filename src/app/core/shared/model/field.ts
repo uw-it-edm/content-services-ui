25ddc07065157b34659af77cbc635f6ec3509bbd
Andrew Gagne <agagne@uw.edu>
2017-11-09 11:18:59 -0800

    CAB-2636: Enable the disabling of sorting on search grid columns

--- src/app/core/shared/model/field.ts
+++ src/app/core/shared/model/field.ts
@@ -1,9 +1,12 @@
 export class Field {
   public name: string;
   public label: string;
+
   public dataType?: string;
   public displayType?: string;
 
   public options?: any[] = [];
   public filteredOptions?: any = [];
+
+  public sortable?: boolean;
 }
