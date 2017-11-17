9cc9553dff083545eff76b15731ac0ca45e7db43
Andrew Gagne <agagne@uw.edu>
2017-11-17 15:27:03 -0800

    CAB-2635: UUI: Display student info as Name (ID)

---
+++ src/app/shared/widgets/student-display/student-display.component.spec.ts
@@ -0,0 +1,66 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StudentDisplayComponent } from './student-display.component';
+import { StudentService } from '../../providers/student.service';
+import { Observable } from 'rxjs/Observable';
+import { Student } from '../../shared/model/student';
+import { By } from '@angular/platform-browser';
+
+class MockStudentService extends StudentService {
+  constructor() {
+    super(null, null);
+  }
+
+  read(studentNumber: string) {
+    const testStudent = new Student();
+    testStudent.displayName = 'Test User';
+    testStudent.firstName = 'Test';
+    testStudent.lastName = 'User';
+    testStudent.studentNumber = '1234';
+    return Observable.of(testStudent);
+  }
+}
+
+describe('StudentDisplayComponent', () => {
+  let component: StudentDisplayComponent;
+  let fixture: ComponentFixture<StudentDisplayComponent>;
+
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        declarations: [StudentDisplayComponent],
+        providers: [{ provide: StudentService, useValue: new MockStudentService() }]
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StudentDisplayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should look up a student id', () => {
+    component.value = '1234';
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(component.displayName).toBe('User, Test (1234)');
+
+    const el = fixture.debugElement.query(By.css('span'));
+    expect(el.nativeElement.innerHTML).toBe('User, Test (1234)');
+    expect(el.nativeElement.classList).not.toContain('invalid-student-number');
+  });
+  it('should display the value, and add the "invalid-student-number" class if it is not a number', () => {
+    component.value = 'a test';
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(component.displayName).toBe('a test');
+
+    const el = fixture.debugElement.query(By.css('span'));
+    expect(el.nativeElement.innerHTML).toBe('a test');
+    expect(el.nativeElement.classList).toContain('invalid-student-number');
+  });
+});
