cbc19dd6f51d074cae6b54f0c146340e3953b6c7
James Renfro <jamesrenfro@users.noreply.github.com>
2017-12-04 13:50:15 -0800

    CAB-2620: Adding more tests for content toolbar

--- src/app/content/content-toolbar/content-toolbar.component.spec.ts
+++ src/app/content/content-toolbar/content-toolbar.component.spec.ts
@@ -28,4 +28,50 @@ describe('ContentToolbarComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not set the page number higher than the page count', () => {
+    component.pageNumber = 8;
+    component.changePageNumber('109');
+    expect(component.pageNumber).toBe(8);
+  });
+
+  it('should notify when page number is changed', () => {
+    const payNumberChangeSpy = spyOn(component.pageNumberChange, 'emit');
+    component.pageNumber = 5;
+    expect(payNumberChangeSpy).toHaveBeenCalled();
+    component.changePageNumber('9');
+    expect(component.pageNumber).toBe(9);
+    expect(payNumberChangeSpy).toHaveBeenCalledTimes(2);
+  });
+
+  it('should respond to page up and page down commands', () => {
+    component.pageNumber = 1;
+    component.pageDown();
+    expect(component.pageNumber).toBe(2);
+    component.pageUp();
+    expect(component.pageNumber).toBe(1);
+  });
+
+  it('should toggle full screen correctly', () => {
+    const fullScreenChangeSpy = spyOn(component.fullScreenChange, 'emit');
+    component.toggleFullScreen();
+    expect(fullScreenChangeSpy).toHaveBeenCalled();
+    expect(component.fullScreen).toBeTruthy();
+    component.toggleFullScreen();
+    expect(fullScreenChangeSpy).toHaveBeenCalledTimes(2);
+    expect(component.fullScreen).toBeFalsy();
+  });
+
+  it('should toggle show all correctly', () => {
+    component.toggleShowAll();
+    expect(component.showAll).toBeTruthy();
+    component.toggleShowAll();
+    expect(component.showAll).toBeFalsy();
+  });
+
+  it('should emit when zoom factor changes', () => {
+    const zoomChangeSpy = spyOn(component.zoom, 'emit');
+    component.changeZoomFactor('0.25');
+    expect(zoomChangeSpy).toHaveBeenCalled();
+  });
 });
