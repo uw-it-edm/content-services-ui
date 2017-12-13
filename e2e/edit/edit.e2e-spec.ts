bacf1728fd8cd638c981aba5993e2cac9c210881
huiaic <huiaic@uw.edu>
2017-12-12 16:31:20 -0800

    add test to check navigation from Edit page back to Search

--- e2e/edit/edit.e2e-spec.ts
+++ e2e/edit/edit.e2e-spec.ts
@@ -1,8 +1,21 @@
 import { EditPage } from './edit.po';
+import { browser } from 'protractor';
+import { SearchPage } from '../search/search.po';
+
+const getCurrentUrl = function() {
+  return browser.getCurrentUrl().then(url => {
+    return url.toLowerCase();
+  });
+};
+
+const getSearchPageUrl = function() {
+  const searchPage = new SearchPage();
+  return searchPage.pageUrl;
+};
 
 describe('content-services-ui Edit Page', () => {
   let page: EditPage;
-  const TITLE = 'Edit Content Item';
+  const demoConfig = require('../mocks/profile-api/demo.json');
 
   beforeAll(() => {
     page = new EditPage();
@@ -10,6 +23,12 @@ describe('content-services-ui Edit Page', () => {
   });
 
   it('should display page title', () => {
-    expect(page.getPageTitle()).toEqual(TITLE);
+    expect(page.getPageTitle()).toEqual(demoConfig.pages.edit.pageName);
+  });
+
+  it('should navigate to Search page when Return to Results link is clicked', () => {
+    page.clickReturnToResultsLink();
+
+    expect(getCurrentUrl()).toMatch(getSearchPageUrl());
   });
 });
