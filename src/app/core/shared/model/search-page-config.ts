e4e8a34bd1a96ba64ad90e7443a413ca1c604d0b
Andrew Gagne <agagne@uw.edu>
2018-01-19 10:00:54 -0800

    CAB-2701: UU: Search by student name or number

--- src/app/core/shared/model/search-page-config.ts
+++ src/app/core/shared/model/search-page-config.ts
@@ -2,6 +2,7 @@ import { FacetsConfig } from './facets-config';
 import { SearchConfig } from './search-config';
 import { Field } from './field';
 import { PageConfig } from './page-config';
+import { SearchAutocompleteConfig } from './search-autocomplete-config';
 
 export class SearchPageConfig implements PageConfig {
   pageName: string;
@@ -11,4 +12,5 @@ export class SearchPageConfig implements PageConfig {
 
   facetsConfig: FacetsConfig = new FacetsConfig();
   searchConfig: SearchConfig = new SearchConfig();
+  autocompleteConfig: SearchAutocompleteConfig;
 }
--- src/app/search/shared/model/search-filter.ts
+++ src/app/search/shared/model/search-filter.ts
@@ -3,16 +3,18 @@ import { isDefined } from '@angular/compiler/src/util';
 export class SearchFilter {
   key: string;
   value: string;
+  label: string;
 
-  constructor(key: string, value: string) {
+  constructor(key: string, value: string, label: string) {
     this.key = key;
     this.value = value;
+    this.label = label;
   }
 
   equals(b: SearchFilter): boolean {
     if (!b) {
       return false;
     }
-    return this.key === b.key && this.value === b.value;
+    return this.key === b.key && this.value === b.value && this.label === b.label;
   }
 }
