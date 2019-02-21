058333c5cffb596f9d58476fb512818fe5c98a17
Maxime Deravet <maximede@users.noreply.github.com>
2019-02-21 15:06:30 -0800

    Increase page size for data-api (#437)

--- src/app/shared/providers/dataapivalue.service.ts
+++ src/app/shared/providers/dataapivalue.service.ts
@@ -12,12 +12,15 @@ import { DataApiValueSearchResults } from '../shared/model/data-api-value-search
 @Injectable()
 export class DataApiValueService {
   private valueUrl = environment.data_api.url + environment.data_api.valueContext;
+  private DATA_API_VALUE_PAGE_SIZE = '300';
 
   constructor(private http: HttpClient, private userService: UserService) {}
 
   @CacheObservableDecorator
   public listByType(type: string): Observable<DataApiValueSearchResults> {
-    const options = this.buildRequestOptions();
+    const params = new HttpParams();
+    params.set('size', this.DATA_API_VALUE_PAGE_SIZE);
+    const options = this.buildRequestOptions(params);
 
     return this.http.get<DataApiValueSearchResults>(this.valueUrl + '/' + type, options);
   }
