de60798f4fee2a47300b2eaa7403026f6f1aec11
Maxime Deravet <maximede@users.noreply.github.com>
2019-04-24 14:07:52 -0700

    CAB-3568 - Allow to display a label for data-api values

--- src/app/core/util/object-utilities.spec.ts
+++ src/app/core/util/object-utilities.spec.ts
@@ -3,14 +3,26 @@ import { ObjectUtilities } from './object-utilities';
 describe('ObjectUtilities', () => {
   const data = { label: 'my-label', metadata: { code: 'my-code' } };
   it('should return a non nested value', () => {
-    expect(ObjectUtilities.getNestedObject(data, ['label'])).toEqual('my-label');
+    expect(ObjectUtilities.getNestedObjectFromArrayOfPath(data, ['label'])).toEqual('my-label');
   });
 
   it('should return a nested value', () => {
-    expect(ObjectUtilities.getNestedObject(data, ['metadata', 'code'])).toEqual('my-code');
+    expect(ObjectUtilities.getNestedObjectFromArrayOfPath(data, ['metadata', 'code'])).toEqual('my-code');
   });
 
   it('should return undefined on unknown fields', () => {
-    expect(ObjectUtilities.getNestedObject(data, ['blw', 'code'])).toBeUndefined();
+    expect(ObjectUtilities.getNestedObjectFromArrayOfPath(data, ['blw', 'code'])).toBeUndefined();
+  });
+
+  it('should return a non nested value', () => {
+    expect(ObjectUtilities.getNestedObjectFromStringPath(data, 'label')).toEqual('my-label');
+  });
+
+  it('should return a nested value', () => {
+    expect(ObjectUtilities.getNestedObjectFromStringPath(data, 'metadata.code')).toEqual('my-code');
+  });
+
+  it('should return undefined on unknown fields', () => {
+    expect(ObjectUtilities.getNestedObjectFromStringPath(data, 'blw.code')).toBeUndefined();
   });
 });
---
+++ src/app/shared/widgets/data-api-display/data-api-value-display.component.spec.ts
@@ -0,0 +1,88 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { NotificationOptions, NotificationService } from '../../providers/notification.service';
+import { MatSnackBarModule } from '@angular/material';
+import { of, throwError } from 'rxjs';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { DataApiValueService } from '../../providers/dataapivalue.service';
+import { DataApiValue } from '../../shared/model/data-api-value';
+import { DataApiValueDisplayComponent } from './data-api-value-display.component';
+
+class NoopNotificationService extends NotificationService {
+  constructor() {
+    super(null);
+  }
+
+  error(message: string, detailedMessage?: any, options: NotificationOptions = new NotificationOptions()) {}
+
+  info(message: string, detailedMessage?: any, options: NotificationOptions = new NotificationOptions(2000)) {}
+
+  warn(message: string, detailedMessage?: any, options: NotificationOptions = new NotificationOptions()) {}
+}
+
+class MockDataApiValueService extends DataApiValueService {
+  constructor() {
+    super(null, null);
+  }
+
+  getByTypeAndValueId(type: string, value: string) {
+    if (value === 'ABCD' && type === 'my-type') {
+      const testDataApiValue = new DataApiValue();
+      testDataApiValue.valueId = 'ABCD';
+      testDataApiValue.data = { metadata: { label: 'Label of ABCD' } };
+      return of(testDataApiValue);
+    } else {
+      return throwError({ message: 'error' });
+    }
+  }
+}
+
+describe('PersonDisplayComponent', () => {
+  let component: DataApiValueDisplayComponent;
+  let fixture: ComponentFixture<DataApiValueDisplayComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [MatSnackBarModule, NoopAnimationsModule],
+      declarations: [DataApiValueDisplayComponent],
+      providers: [
+        { provide: NotificationService, useValue: new NoopNotificationService() },
+        { provide: DataApiValueService, useValue: new MockDataApiValueService() }
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DataApiValueDisplayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should look up a dataApiValue', () => {
+    component.value = 'ABCD';
+    component.type = 'my-type';
+    component.labelPath = 'metadata.label';
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(component.displayValue).toBe('Label of ABCD');
+
+    const el = fixture.debugElement.query(By.css('span'));
+    expect(el.nativeElement.innerHTML).toBe('Label of ABCD');
+    expect(el.nativeElement.classList).not.toContain('invalid-regid');
+  });
+  it('should display the value, and add the "invalid-valueid" class if it is not a valid valueId', () => {
+    component.value = 'AKDSAKD';
+    component.ngOnInit();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(component.displayValue).toBe('AKDSAKD');
+
+      const el = fixture.debugElement.query(By.css('span'));
+      expect(el.nativeElement.innerHTML).toBe('AKDSAKD');
+      expect(el.nativeElement.classList).toContain('invalid-valueid');
+    });
+  });
+});
