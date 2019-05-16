7863876d0919bcd9e7be7fa250cdd4995cc04c95
Jiefeng Shen <jfshen@uw.edu>
2019-05-16 13:13:49 -0700

    white space formatting

--- src/app/shared/widgets/person-autocomplete/person-autocomplete.component.ts
+++ src/app/shared/widgets/person-autocomplete/person-autocomplete.component.ts
@@ -126,7 +126,7 @@ export class PersonAutocompleteComponent extends _PersonAutocompleteComponentBas
                   this.filteredOptions = results.content;
                 });
             }
-          } else if (!term || term.trim().length == 0) {
+          } else if (!term || term.trim().length === 0) {
             // if empty, the user probably wants to delete the value
             this._propagateChanges(null);
           }
