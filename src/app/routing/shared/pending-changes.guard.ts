a876a83b97a97e19e40d1d86217ed7f7ce340446
Andrew Gagne <agagne@uw.edu>
2019-06-18 15:32:50 -0700

    Allow customizable 'unsavedChangesMessage'

--- src/app/routing/shared/pending-changes.guard.ts
+++ src/app/routing/shared/pending-changes.guard.ts
@@ -2,17 +2,42 @@ import { Observable } from 'rxjs';
 
 import { ComponentCanDeactivate } from './component-can-deactivate';
 import { CanDeactivate } from '@angular/router';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { ConfigResolver } from './config-resolver.service';
+import { first } from 'rxjs/operators';
+import { CustomTextUtilities } from '../../shared/directives/custom-text/custom-text-utilities';
+import { CustomTextItem } from '../../core/shared/model/config';
 
 @Injectable()
 export class PendingChangesGuard implements CanDeactivate<ComponentCanDeactivate> {
+  private unsavedChangesMessage;
+  private customText: Map<string, CustomTextItem>;
+
+  constructor(private configResolver: ConfigResolver) {
+    this.init();
+  }
+
   canDeactivate(component: ComponentCanDeactivate): boolean | Observable<boolean> {
     if (component.canDeactivate()) {
       return true;
     } else {
-      return confirm(
-        'You have unsaved changes. Press Cancel to go back and save these changes, or OK and your changes will be lost.'
-      );
+      return confirm(this.unsavedChangesMessage.label);
     }
+  }
+
+  init(): void {
+    this.configResolver
+      .getCustomTextSubject()
+      .pipe(first())
+      .subscribe(customText => {
+        this.customText = customText;
+      });
+    const defaultText =
+      'You have unsaved changes. Press Cancel to go back and save these changes, or OK and your changes will be lost.';
+    this.unsavedChangesMessage = CustomTextUtilities.getCustomText(
+      this.customText,
+      'unsavedChangesMessage',
+      defaultText
+    );
   }
 }
