d3af6e983903c628cd1201ae60fa325232156490
Andrew Gagne <agagne@gmail.com>
2019-07-02 15:48:10 -0700

    Feature/cab 3569 (#629)
    * CAB-3569: UUI - Limit date ranges to dates between year 1653 and 2286
    
    - add elasticsearch-date-validator.directive
    - add new custom text: 'timePickerRequired', 'timePickerInvalidDate', 'timePickerElasticsearchDateValidator'

---
+++ src/app/shared/directives/elasticsearch-date-validator/elasticsearch-date-validator.directive.spec.ts
@@ -0,0 +1,40 @@
+import { ElasticsearchDateValidatorDirective } from './elasticsearch-date-validator.directive';
+import { FormControl, ValidationErrors } from '@angular/forms';
+
+describe('ElasticsearchDateValidatorDirective', () => {
+  const validationErrors: ValidationErrors = {
+    years: {
+      message: 'The year must be a valid date between 1653 and 2285'
+    }
+  };
+
+  it('should create an instance', () => {
+    const directive = new ElasticsearchDateValidatorDirective();
+    expect(directive).toBeTruthy();
+  });
+
+  it('should return null for valid dates between 1653 & 2285', () => {
+    const errorMessage = ElasticsearchDateValidatorDirective.validateTimestamp(1562085173000);
+    expect(errorMessage).toBeNull();
+  });
+
+  it('validation error should be returned for timestamp that is less than 1653', () => {
+    const result = ElasticsearchDateValidatorDirective.validateTimestamp(-24283006027888);
+    expect(result).toEqual(validationErrors);
+  });
+
+  it('validation error should be returned for timestamp that is greater than 2285 ', () => {
+    const result = ElasticsearchDateValidatorDirective.validateTimestamp(9987813173000);
+    expect(result).toEqual(validationErrors);
+  });
+
+  it('validation error should be returned for control with a value that is greater than 2285 ', () => {
+    const result = ElasticsearchDateValidatorDirective.validateControl(new FormControl(9987813173000));
+    expect(result).toEqual(validationErrors);
+  });
+
+  it('null should be returned for control with a valid value ', () => {
+    const result = ElasticsearchDateValidatorDirective.validateControl(new FormControl(1562085173000));
+    expect(result).toBeNull();
+  });
+});
