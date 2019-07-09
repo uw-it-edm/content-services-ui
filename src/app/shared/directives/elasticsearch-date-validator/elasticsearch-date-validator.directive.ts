3b09771004d2c161ba44fa4d81c233436eeadc52
Huiai Chan <30538411+huiaic@users.noreply.github.com>
2019-07-09 15:56:50 -0700

    Update elasticsearch-date-validator.directive.ts
    update comments

--- src/app/shared/directives/elasticsearch-date-validator/elasticsearch-date-validator.directive.ts
+++ src/app/shared/directives/elasticsearch-date-validator/elasticsearch-date-validator.directive.ts
@@ -9,7 +9,7 @@ import { isNullOrUndefined } from '../../../core/util/node-utilities';
 export class ElasticsearchDateValidatorDirective implements Validator {
   /*
    * https://www.elastic.co/guide/en/elasticsearch/reference/2.4/mapping-date-format.html
-   * "timestamp allows a max length of 13 chars, so only dates between 1653 and 2286 are supported. "
+   * "timestamp allows a max length of 13 chars, so only dates between 1654 and 2286 are supported. "
    * */
   static readonly minDate = new Date(1654, 0, 1);
   static readonly maxDate = new Date(2285, 11, 31);
