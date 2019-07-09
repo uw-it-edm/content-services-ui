74629cb088b3970ae3bfc52db27f8f06292734e4
Huiai Chan <30538411+huiaic@users.noreply.github.com>
2019-07-09 15:53:53 -0700

    Update elasticsearch-date-validator.directive.ts
    updating min date to 1654. search-api doesn't seem to like 1653

--- src/app/shared/directives/elasticsearch-date-validator/elasticsearch-date-validator.directive.ts
+++ src/app/shared/directives/elasticsearch-date-validator/elasticsearch-date-validator.directive.ts
@@ -11,7 +11,7 @@ export class ElasticsearchDateValidatorDirective implements Validator {
    * https://www.elastic.co/guide/en/elasticsearch/reference/2.4/mapping-date-format.html
    * "timestamp allows a max length of 13 chars, so only dates between 1653 and 2286 are supported. "
    * */
-  static readonly minDate = new Date(1653, 0, 1);
+  static readonly minDate = new Date(1654, 0, 1);
   static readonly maxDate = new Date(2285, 11, 31);
 
   static validateControl(control: FormControl): ValidationErrors {
