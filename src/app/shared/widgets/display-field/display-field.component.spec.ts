52bb974929c4347d13a2843b3789547d221a9592
Federico Silva Armas <farmas@uw.edu>
2020-02-04 16:18:25 -0800

    UUI Search page should display 4 digit year for Dates and DateTimes
    Fixes CAB-3686

--- src/app/shared/widgets/display-field/display-field.component.spec.ts
+++ src/app/shared/widgets/display-field/display-field.component.spec.ts
@@ -3,7 +3,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DisplayFieldComponent } from './display-field.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { By } from '@angular/platform-browser';
-import { DatePipe } from '@angular/common';
 import { Field } from '../../../core/shared/model/field';
 
 describe('DisplayFieldComponent', () => {
@@ -35,10 +34,10 @@ describe('DisplayFieldComponent', () => {
     const span = fixture.debugElement.query(By.css('span span'));
     const el = span.nativeElement;
 
-    expect(el.innerHTML.trim()).toBe('11/21/17');
+    expect(el.innerHTML.trim()).toBe('11/21/2017');
   });
   it('should display a dateTime', () => {
-    const date = new Date();
+    const date = new Date('1995-12-17T03:24:00');
 
     component.value = date.getTime();
     component.field = Object.assign(new Field(), { key: 'displayDate', label: 'displayDate', displayType: 'dateTime' });
@@ -48,9 +47,7 @@ describe('DisplayFieldComponent', () => {
     const span = fixture.debugElement.query(By.css('span span'));
     const el = span.nativeElement;
 
-    const datePipe = new DatePipe('en');
-    const dateTimeString: string = datePipe.transform(date, 'short');
-    expect(el.innerHTML.trim()).toBe(dateTimeString);
+    expect(el.innerHTML.trim()).toBe('12/17/1995, 3:24 AM');
   });
   it('should use the student-display componenent when a student', () => {
     component.value = '1234';
