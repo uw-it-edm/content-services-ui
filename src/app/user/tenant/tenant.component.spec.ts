7098e4dd71b0a316bb963cfb7a594fe6c1fc95e8
Andrew Gagne <agagne@uw.edu>
2020-03-27 16:07:07 -0700

    remove mistakenly committed whitespace changes

--- src/app/user/tenant/tenant.component.spec.ts
+++ src/app/user/tenant/tenant.component.spec.ts
@@ -1,13 +1,13 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {TenantComponent} from './tenant.component';
-import {NO_ERRORS_SCHEMA} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {MaterialConfigModule} from '../../routing/material-config.module';
-import {ActivatedRouteStub} from '../../../testing/router-stubs';
-import {Title} from '@angular/platform-browser';
-import {Config} from '../../core/shared/model/config';
-import {GlobalEventsManagerService} from '../../core/shared/global-events-manager.service';
+import { TenantComponent } from './tenant.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MaterialConfigModule } from '../../routing/material-config.module';
+import { ActivatedRouteStub } from '../../../testing/router-stubs';
+import { Title } from '@angular/platform-browser';
+import { Config } from '../../core/shared/model/config';
+import { GlobalEventsManagerService } from '../../core/shared/global-events-manager.service';
 
 let activatedRoute: ActivatedRouteStub;
 let component: TenantComponent;
@@ -20,14 +20,14 @@ describe('TenantComponent', () => {
     TestBed.configureTestingModule({
       imports: [MaterialConfigModule],
       declarations: [TenantComponent],
-      providers: [{provide: ActivatedRoute, useValue: activatedRoute}, Title, GlobalEventsManagerService],
+      providers: [{ provide: ActivatedRoute, useValue: activatedRoute }, Title, GlobalEventsManagerService],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
 
-    activatedRoute.testParamMap = {tenant: 'test-tenant'};
+    activatedRoute.testParamMap = { tenant: 'test-tenant' };
     const config = new Config();
     config.tenant = 'test-tenant';
-    activatedRoute.testData = {config: config};
+    activatedRoute.testData = { config: config };
 
     fixture = TestBed.createComponent(TenantComponent);
     component = fixture.componentInstance;
@@ -52,5 +52,4 @@ describe('TenantComponent', () => {
     const title = fixture.debugElement.injector.get(Title);
     expect(title.getTitle()).toBe('test-tenant');
   });
-
 });
