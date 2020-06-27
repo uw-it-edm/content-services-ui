c94719eb44e652d8b395d0a47cf62159b643b7ff
Federico Silva Armas <farmas@uw.edu>
2020-06-26 17:09:53 -0700

    CAB-4073: E2E tests for cascading field validators for Bulk Update.

--- e2e/bulk-edit/bulk-edit.local-spec.ts
+++ e2e/bulk-edit/bulk-edit.local-spec.ts
@@ -3,19 +3,21 @@ import { SearchPage } from '../search/search.po';
 import { ContentServicesUiPage } from '../app/app.po';
 import { BrowserUtils } from '../browserUtils';
 
-describe('Bulk Update Flow', () => {
-  const editPage = new BulkEditPage();
-  const searchPage = new SearchPage();
-  const utils = new BrowserUtils();
+const editPage = new BulkEditPage();
+const searchPage = new SearchPage();
+const utils = new BrowserUtils();
 
+describe('Bulk Update Page', () => {
   it('should have no accessibility violations', () => {
     editPage.navigateTo();
 
     const app = new ContentServicesUiPage();
     app.runAccessibilityChecks();
   });
+});
 
-  it('should allow user to update multiple documents at once', () => {
+describe('Bulk Update Flow', () => {
+  beforeEach(() => {
     searchPage.navigateTo();
 
     // enter bulk edit mode in search page
@@ -27,12 +29,14 @@ describe('Bulk Update Flow', () => {
     utils.waitForElementText(searchPage.bulkUpdateNextButton, 'Edit 3 Documents');
     searchPage.bulkUpdateNextButton.click();
     utils.waitForElementText('h2', 'Demo | Bulk Update');
+  });
 
+  it('should allow user to update multiple documents at once', () => {
     // verify the 3 rows are displayed on the edit page
     expect(editPage.searchResults.getResultsByColumn('id')).toEqual(['123', '234', '456']);
 
     // set a field value and update
-    editPage.inputFields.get(0).sendKeys('test value');
+    editPage.fields.inputElements.get(0).sendKeys('test value');
     editPage.updateButton.click();
 
     // Wait for all rows to be gone and verify success message
@@ -42,5 +46,37 @@ describe('Bulk Update Flow', () => {
     // Close the bulk update mode and verify return to search page.
     editPage.cancelButton.click();
     utils.waitForElementText('h2', 'Demonstration Search');
+  });
+
+  it('should add required validator if parent cascading select has value and child does not', () => {
+    const parentField = editPage.fields.getSelectFormField('DataApiOption parent');
+    const childField = editPage.fields.getSelectFormField('DataApiOption child');
+
+    // verify child is not required on load.
+    expect(childField.optionsInputElement.getAttribute('required')).toBeFalsy();
+
+    // select value of parent, verify child is now required
+    parentField.selectValue(1);
+    expect(childField.optionsInputElement.getAttribute('required')).toBeTruthy();
+
+    // select value of child, verify child validator is removed
+    childField.selectValue(1);
+    expect(childField.optionsInputElement.getAttribute('required')).toBeFalsy();
+  });
+
+  it('should remove child required validator when fields are reset', () => {
+    const parentField = editPage.fields.getSelectFormField('DataApiOption parent');
+    const childField = editPage.fields.getSelectFormField('DataApiOption child');
+
+    // verify child is not required on load.
+    expect(childField.optionsInputElement.getAttribute('required')).toBeFalsy();
+
+    // select value of parent, verify child is now required
+    parentField.selectValue(1);
+    expect(childField.optionsInputElement.getAttribute('required')).toBeTruthy();
+
+    // reset fields, verify child validator is removed.
+    editPage.resetButton.click();
+    expect(childField.optionsInputElement.getAttribute('required')).toBeFalsy();
   });
 });
