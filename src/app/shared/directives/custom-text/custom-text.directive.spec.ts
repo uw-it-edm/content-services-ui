f8430bd66ec9944b8db8d1c2d1890ff14c96dcd2
Federico Silva Armas <farmas@uw.edu>
2020-07-31 15:29:36 -0700

    CAB-3993: Accessibility: Screen Reader announced "undefined" for Facets.

--- src/app/shared/directives/custom-text/custom-text.directive.spec.ts
+++ src/app/shared/directives/custom-text/custom-text.directive.spec.ts
@@ -14,11 +14,14 @@ class RouterStub {
 }
 
 class ConfigResolverStub {
+  description = 'Test Description';
+  label = 'Test Label';
+
   getCustomTextSubject(): Observable<Map<string, CustomTextItem>> {
     const customText = new Map<string, CustomTextItem>();
     const testButton = new CustomTextItem();
-    testButton.label = 'Test Label';
-    testButton.description = 'Test Description';
+    testButton.label = this.label;
+    testButton.description = this.description;
     customText['testButton'] = testButton;
 
     return of(customText);
@@ -27,50 +30,65 @@ class ConfigResolverStub {
 
 @Component({
   selector: 'app-test-component',
-  template: '<div appCustomText="testButton" defaultValue="none"></div>'
+  template: '<div appCustomText="testButton" defaultValue="none"></div>',
 })
 class TestComponent {}
 
 describe('CustomTextDirective', () => {
+  let configResolverStub: ConfigResolverStub;
+
   beforeEach(async(() => {
-    const _configResolverStub = new ConfigResolverStub();
+    configResolverStub = new ConfigResolverStub();
 
     TestBed.configureTestingModule({
       imports: [],
       declarations: [CustomTextDirective, TestComponent],
       providers: [
-        { provide: ConfigResolver, useValue: _configResolverStub },
-        { provide: Router, useClass: RouterStub }
+        { provide: ConfigResolver, useValue: configResolverStub },
+        { provide: Router, useClass: RouterStub },
       ],
-      schemas: [NO_ERRORS_SCHEMA]
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
 
   let configResolverService: ConfigResolverStub;
-  beforeEach(() => {
-    inject([Injector], injector => {
+  beforeEach(async(() => {
+    inject([Injector], (injector) => {
       configResolverService = injector.get(ConfigResolver);
     });
-  });
 
-  it('should replace text and title attribute correctly', async(() => {
     TestBed.overrideComponent(TestComponent, {
       set: {
-        template: '<div appCustomText="testButton" defaultValue="none"></div>'
-      }
+        template: '<div appCustomText="testButton" defaultValue="none"></div>',
+      },
     });
 
-    TestBed.compileComponents().then(() => {
-      const fixture = TestBed.createComponent(TestComponent);
-      fixture.detectChanges();
-      const directiveEl = fixture.debugElement.query(By.directive(CustomTextDirective));
-      expect(directiveEl).not.toBeNull();
-
-      const directiveInstance = directiveEl.injector.get(CustomTextDirective);
-      expect(directiveInstance.appCustomText).toBe('testButton');
-
-      expect(fixture.debugElement.nativeElement.innerHTML).toContain('Test Label');
-      expect(directiveEl.attributes.title).toBe('Test Description');
-    });
+    return TestBed.compileComponents();
   }));
+
+  it('should replace text and title attribute correctly', () => {
+    const fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+    const directiveEl = fixture.debugElement.query(By.directive(CustomTextDirective));
+    expect(directiveEl).not.toBeNull();
+
+    const directiveInstance = directiveEl.injector.get(CustomTextDirective);
+    expect(directiveInstance.appCustomText).toBe('testButton');
+
+    expect(fixture.debugElement.nativeElement.innerHTML).toContain('Test Label');
+    expect(directiveEl.attributes.title).toBe('Test Description');
+  });
+
+  it('should not add title attribute if description is not defined', () => {
+    configResolverStub.description = null;
+
+    const fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+
+    const directiveEl = fixture.debugElement.query(By.directive(CustomTextDirective));
+    const directiveInstance = directiveEl.injector.get(CustomTextDirective);
+    expect(directiveInstance.appCustomText).toBe('testButton');
+    expect(fixture.debugElement.nativeElement.innerHTML).toContain('Test Label');
+    expect(directiveEl.attributes.title).toBeUndefined();
+  });
 });
